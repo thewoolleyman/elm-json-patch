bba74ee0a9a595f9d81cfdb784dd566c796f785d
Chad Woolley <thewoolleyman@gmail.com>
2016-08-09 20:04:54 -0700

    framework and tests

---
+++ src/JsonPatch/Reverser.elm
@@ -0,0 +1,6 @@
+module JsonPatch.Reverser exposing (reverseIt)
+
+import String exposing (reverse)
+
+reverseIt : String -> String
+reverseIt str = reverse str
---
+++ tests/Test/JsonPatchTest.elm
@@ -0,0 +1,24 @@
+module Test.JsonPatchTest exposing (..)
+
+import Test exposing (..)
+import Expect
+import Fuzz exposing (..)
+import String exposing (reverse)
+
+import JsonPatch.Reverser exposing (reverseIt)
+
+reverser =
+  describe "Reverser reverseIt"
+    [ describe "test"
+      [ test "reverses" <|
+        \() ->
+          reverseIt "Missy Elliott"
+            |> Expect.equal "ttoillE yssiM"
+      ]
+    , describe "fuzz"
+      [ fuzzWith { runs = 100 } ( string ) "reverses" <|
+        \ str ->
+          reverseIt str
+            |> Expect.equal (reverse str)
+      ]
+    ]
