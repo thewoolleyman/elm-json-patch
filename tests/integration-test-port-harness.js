437b7f085fbed62bfcde1603472a1f89b09179db
Chad Woolley <thewoolleyman@gmail.com>
2016-08-14 21:19:39 -0700

    simple pass/fail for integration test

--- tests/integration-test-port-harness.js
+++ tests/integration-test-port-harness.js
@@ -1,8 +1,14 @@
-var ports = Elm.Test.Integration.PortParserTest.worker().ports;
+const ports = Elm.Test.Integration.PortParserTest.worker().ports;
+
+const expected = "This is a JSON patch";
+var $expected = document.getElementById('expected');
+$expected.innerText = JSON.stringify(expected);
 
 ports.displayModel.subscribe(function(model) {
-  var $el = document.getElementById('model');
-  $el.innerText = JSON.stringify(model);
+  var $model = document.getElementById('model');
+  $model.innerText = JSON.stringify(model);
+  var $result = document.getElementById('result');
+  $result.innerText = (expected == model);
 });
 
 setTimeout(function () {
